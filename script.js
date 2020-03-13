d3120d94691756599cd68cbefcb0dc2276a0add7
AlexeyTeterin <teterin.alexey@gmail.com>
2020-03-13 14:11:50 +0500

    refactor: add smoottScroll var

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const MENU = document.querySelector('.menu');
 var selectedID = 'menu-home';
+var smoothScroll = {
+  block: "start",
+  behavior: "smooth"
+};
 
 MENU.addEventListener('click', (event) => {
   let id = event.target.id;
@@ -12,72 +16,58 @@ MENU.addEventListener('click', (event) => {
 
   switch (true) {
     case (selectedID === 'menu-home'): {
-      document.querySelector('div.slider').scrollIntoView({
-        block: 'start',
-        behavior: "smooth",
-      });
+      document.querySelector('div.slider').scrollIntoView(smoothScroll);
       break;
     }
     case (selectedID === 'menu-serv'): {
-      document.querySelector('div.services').scrollIntoView({
-        block: "start",
-        behavior: "smooth",
-      });
+      document.querySelector('div.services').scrollIntoView(smoothScroll);
       break;
     }
     case (selectedID === 'menu-about'): {
-      document.querySelector('div.about').scrollIntoView({
-        block: "start",
-        behavior: "smooth"
-      });
+      document.querySelector('div.about').scrollIntoView(smoothScroll);
       break;
     }
     case (selectedID === 'menu-portf'): {
-      document.querySelector('div.portfolio').scrollIntoView({
-        block: "start",
-        behavior: "smooth"
-      });
+      document.querySelector('div.portfolio').scrollIntoView(smoothScroll);
       break;
     }
     case (selectedID === 'menu-contact'): {
-      document.querySelector('div.feedback').scrollIntoView({
-        block: "start",
-        behavior: "smooth"
-      });
+      document.querySelector('div.feedback').scrollIntoView(smoothScroll);
       break;
     }
   }
 });
 
-window.addEventListener('load', function () {
+window.addEventListener('load', () => {
   if (window.pageYOffset === 0) {
     document.querySelector('#menu-home').classList.add('active');
   }
 });
 
 window.addEventListener('scroll', function () {
+
   if (window.pageYOffset < 500) {
-    if (selectedID !== undefined) document.getElementById(selectedID).classList.remove('active');
+    document.getElementById(selectedID).classList.remove('active');
     document.getElementById('menu-home').classList.add('active');
     selectedID = 'menu-home';
   }
   if (window.pageYOffset >= 400) {
-    if (selectedID !== undefined) document.getElementById(selectedID).classList.remove('active');
+    document.getElementById(selectedID).classList.remove('active');
     document.getElementById('menu-serv').classList.add('active');
     selectedID = 'menu-serv';
   }
   if (window.pageYOffset >= 900) {
-    if (selectedID !== undefined) document.getElementById(selectedID).className = "";
+    document.getElementById(selectedID).className = "";
     document.getElementById('menu-portf').className = "active";
     selectedID = 'menu-portf';
   }
   if (window.pageYOffset >= 1700) {
-    if (selectedID !== undefined) document.getElementById(selectedID).className = "";
+    document.getElementById(selectedID).className = "";
     document.getElementById('menu-about').className = "active";
     selectedID = 'menu-about';
   }
   if (window.pageYOffset >= 2400 || (window.innerHeight + window.pageYOffset) >= document.body.scrollHeight) {
-    if (selectedID !== undefined) document.getElementById(selectedID).className = "";
+    document.getElementById(selectedID).className = "";
     document.getElementById('menu-contact').className = "active";
     selectedID = 'menu-contact';
   }
